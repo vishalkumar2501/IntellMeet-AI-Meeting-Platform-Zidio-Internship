7eca736921304f2d9aa2ce13ce8f0372f5a9503b
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-04-25 23:01:33 +0530

    nav code

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <div style={{
+      background: "#2874f0",
+      color: "white",
+      padding: "10px",
+      fontSize: "20px"
+    }}>
+      Flipkart Clone
+    </div>
+  );
+}
+
+export default Navbar;
