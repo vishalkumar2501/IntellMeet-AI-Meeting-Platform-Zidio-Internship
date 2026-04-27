784aa9bfce384f5601d411d5c008afe38eed4227
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-04-27 19:25:10 +0530

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 function Home() {
-  return (
-    <div>
-      <h2>Welcome to Flipkart Clone</h2>
-    </div>
-  );
-}
-
-export default Home;
+  const products = [
+    {
+      id: 1,
+      name: "iPhone 14",
+      price: "₹70,000",
+      image: "https://via.placeholder.com/150"
+    },
+    {
+      id: 2,
+      name: "Samsung Galaxy",
+      price: "₹50,000",
+      image: "https://via.placeholder.com/150"
+    },
