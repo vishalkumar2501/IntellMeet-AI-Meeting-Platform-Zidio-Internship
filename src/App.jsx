70935b3943ca8766784c0786fd3dd6b52bee48ee
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-04-28 22:19:39 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
   return (
     <div>
-      <Navbar />
-      <Home />
+      <Navbar cartCount={cart.length} />
+      <Home addToCart={addToCart} />
     </div>
   );
 }
