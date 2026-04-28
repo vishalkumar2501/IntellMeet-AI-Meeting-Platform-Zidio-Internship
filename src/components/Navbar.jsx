9d4098816acea8b41d2d24bcafc27d922a725b67
Vishal Kumar <vishalkumar2501kvs@gmail.com>
2026-04-28 22:21:37 +0530

    changes in navbaar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,28 @@
-import { useState } from "react";
-
-function Navbar() {
-  const [search, setSearch] = useState("");
-
+function Navbar({ cartCount }) {
   return (
     <div style={{
       display: "flex",
-      alignItems: "center",
       justifyContent: "space-between",
       background: "#2874f0",
       padding: "10px 20px",
       color: "white"
     }}>
+      <h2>Flipkart</h2>
 
-      {/* Logo */}
-      <h2 style={{ margin: 0 }}>Flipkart</h2>
-
-      {/* Search Bar */}
-      <input
-        type="text"
-        placeholder="Search for products"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        style={{
-          width: "40%",
-          padding: "8px",
-          borderRadius: "2px",
-          border: "none"
-        }}
-      />
-
-      {/* Buttons */}
       <div>
         <button style={{
-          padding: "8px 15px",
           marginRight: "10px",
-          background: "white",
-          color: "#2874f0",
-          border: "none",
-          cursor: "pointer"
+          padding: "5px 10px"
         }}>
           Login
         </button>
 
         <button style={{
-          padding: "8px 15px",
-          background: "white",
-          color: "#2874f0",
-          border: "none",
-          cursor: "pointer"
+          padding: "5px 10px"
         }}>
-          Cart
+          Cart ({cartCount})
         </button>
       </div>
-
     </div>
   );
 }
